d16027f71d4253a59b277b09ba2adde0ff468706
SreejithMahesan <43379081+SreejithMahesan@users.noreply.github.com>
2019-02-01 09:08:46 -0500

    TestNG

---
+++ Learning/src/test/java/Selenium/Learning/TestNGLearning.java
@@ -0,0 +1,37 @@
+package Selenium.Learning;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestNGLearning {
+	
+WebDriver driver;
+	
+	
+	@BeforeTest
+	public void BeforeTest()
+	{
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@Test(priority =0,enabled=true)
+	public void Test01()
+	{
+		System.out.println("This is the first test");
+		
+	}
+	
+	
+	
+	@AfterTest
+	public void teardown()
+	{
+		driver.quit();
+		
+	}	
+
+}
---
+++ Learning/src/test/java/Selenium/Learning/TestNgListnersLearning.java
@@ -0,0 +1,47 @@
+package Selenium.Learning;
+
+
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestNgListnersLearning implements ITestListener {
+
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("The method on start is :" + result.getName());
+		
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
